82dd2d88d09d3f0ffe029b5245b61a9ae0c44bcd
GodHyum <38372691+gusghrlrl101@users.noreply.github.com>
2019-04-09 02:51:37 +0900

    map에서 my로 데이터 전달 해야함

---
+++ app/src/main/java/com/example/mytravelmap/ListViewAdapter.java
@@ -0,0 +1,69 @@
+package com.example.mytravelmap;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.model.Marker;
+
+import java.util.ArrayList;
+
+
+public class ListViewAdapter extends BaseAdapter {
+    // 저장할 데이터
+    private ArrayList<ListViewItem> data = new ArrayList<ListViewItem>();
+
+    // 기본 생성자
+    public ListViewAdapter() {
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //  convertView에 "listview_item" Layout을 inflate 하기
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.listview_item, parent, false);
+        }
+
+        // 해당하는 id의 View 불러오기
+        ImageView imgView = (ImageView) convertView.findViewById(R.id.list_img);
+        TextView titleView = (TextView) convertView.findViewById(R.id.list_title);
+        TextView contentView = (TextView) convertView.findViewById(R.id.list_content);
+
+        // List로부터 Data 가져오기
+        ListViewItem listViewItem = data.get(position);
+
+        // 해당하는 View에 데이터 삽입
+        imgView.setImageDrawable(listViewItem.getImg());
+        titleView.setText(listViewItem.getTitle());
+        contentView.setText(listViewItem.getContent());
+
+        return convertView;
+    }
+
+    // List에 데이터 추가하기
+    public void addData(Drawable img, Marker marker, String title, String content) {
+        ListViewItem item = new ListViewItem(img, marker, title, content);
+        data.add(item);
+    }
+}
--- app/src/main/java/com/example/mytravelmap/MainActivity.java
+++ app/src/main/java/com/example/mytravelmap/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.mytravelmap;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+        implements MapFragment.MapInterface {
+    private ArrayList<ListViewItem> arrayList;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PagerAdapter mPagerAdapter = new PagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mViewPager.setAdapter(mPagerAdapter);
+
+        TabLayout mTab = (TabLayout) findViewById(R.id.tabs);
+        mTab.setupWithViewPager(mViewPager);
+    }
+
+    @Override
+    public void moveInfo(String id) {
+        mViewPager.setCurrentItem(2, true);
     }
 }
